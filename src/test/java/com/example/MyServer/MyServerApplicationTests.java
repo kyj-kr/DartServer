77c8c00f8a944f5463b3f9331b9d0c0ab2953d56
kyj-kr <kyj_kr@naver.com>
2021-08-26 19:31:41 +0900

    First commit in this project :)

---
+++ src/test/java/com/example/MyServer/MyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
