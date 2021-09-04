dcec149bda45d492fff6aaf72c5a74b61f9677fb
kyj-kr <kyj_kr@naver.com>
2021-09-04 19:59:33 +0900

    resolve key path error

--- src/main/java/com/example/MyServer/FBInitialize.java
+++ src/main/java/com/example/MyServer/FBInitialize.java
@@ -14,7 +14,7 @@ public class FBInitialize {
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccountStream = new FileInputStream("resources/dart-1f534-firebase-adminsdk-3ytt9-4c730476d5.json");
+            FileInputStream serviceAccountStream = new FileInputStream("src/main/resources/dart-1f534-firebase-adminsdk-3ytt9-4c730476d5.json");
             FirebaseOptions firebaseOptions = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
                     .setDatabaseUrl("https://dart-1f534-default-rtdb.firebaseio.com/")
