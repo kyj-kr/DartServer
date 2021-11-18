c476d6f470f3160fe0568a43af99a8dbeeb69c6c
kyj-kr <kyj_kr@naver.com>
2021-11-18 19:20:56 +0900

    done :)

--- src/main/java/com/example/MyServer/SendMsgService.java
+++ src/main/java/com/example/MyServer/SendMsgService.java
@@ -5,19 +5,16 @@ import org.apache.http.HttpHeaders;
 
 public class SendMsgService {
 
-    private static final String URL_DART_HEROKU = "https://dart-application.herokuapp.com/token";
+    private static final String URL_DART_HEROKU = "https://dart-application.herokuapp.com";
 
     public void sendMySelf() {
         try {
             OkHttpClient client = new OkHttpClient();
-            RequestBody requestBody = RequestBody.create("", MediaType.get("application/json; charset=utf-8"));
             Request request = new Request.Builder()
                     .url(URL_DART_HEROKU)
-                    .get()
-                    .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
                     .build();
 
-            Response response = client.newCall(request).execute();
+            client.newCall(request).execute();
         } catch(Exception e) {
             e.printStackTrace();
         }
