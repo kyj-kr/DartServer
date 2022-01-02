42dfaac892def858b3257405c247525f22b22a03
kyj-kr <kyj_kr@naver.com>
2022-01-02 18:47:19 +0900

    done :)

--- src/main/java/com/example/MyServer/SendMsgService.java
+++ src/main/java/com/example/MyServer/SendMsgService.java
@@ -8,15 +8,25 @@ public class SendMsgService {
     private static final String URL_DART_HEROKU = "https://dart-application.herokuapp.com";
 
     public void sendMySelf() {
+        Response response = null;
         try {
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     .url(URL_DART_HEROKU)
                     .build();
 
-            client.newCall(request).execute();
+            response = client.newCall(request).execute();
         } catch(Exception e) {
             e.printStackTrace();
+        } finally {
+            if (response != null) {
+                try {
+                    response.body().close();
+                }
+                catch (final Throwable th) {
+                    System.out.println(th.getMessage());
+                }
+            }
         }
     }
 }
